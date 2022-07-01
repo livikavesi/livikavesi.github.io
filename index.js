115a3325874b1e4833ca34f444d414089f2a0d25
livikavesi <livitaufao@gmail.com>
2022-07-01 13:58:47 +1200

    new js nav

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hey")
+let blog = document.getElementById('blog');
+let core = document.getElementById('core');
+let mainImgContainer = document.getElementById('main-img-container');
+let secondImgContainer = document.getElementById('second-img-container');
+console.log(blog);
+
+core.onclick = () => {
+    mainImgContainer.style.display = 'none';
+    secondImgContainer.style.display = 'flex';
+    secondImgContainer.style.justifyContent = 'space-around';
+}
+blog.onclick = () => {
+    secondImgContainer.style.display = 'none';
+    mainImgContainer.style.display = 'flex';
+}
