5318d8f4ec89334b1e7c1556532073bef710406a
livikavesi <livitaufao@gmail.com>
2022-07-01 14:35:10 +1200

    nav home/core/blog js link

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let blog = document.getElementById('blog');
 let core = document.getElementById('core');
+let home = document.getElementById('home');
 let mainImgContainer = document.getElementById('main-img-container');
 let secondImgContainer = document.getElementById('second-img-container');
-console.log(blog);
+let thirdImgContainer = document.getElementById('third-img-container');
 
 core.onclick = () => {
     mainImgContainer.style.display = 'none';
     secondImgContainer.style.display = 'flex';
-    secondImgContainer.style.justifyContent = 'space-around';
+    thirdImgContainer.style.display = 'none';
 }
 blog.onclick = () => {
+    mainImgContainer.style.display = 'none';
     secondImgContainer.style.display = 'none';
+    thirdImgContainer.style.display = 'flex';
+    thirdImgContainer.style.justifyContent = 'space-around';
+}
+home.onclick = () => {
+    secondImgContainer.style.display = 'none';
+    thirdImgContainer.style.display = 'none';
     mainImgContainer.style.display = 'flex';
 }
