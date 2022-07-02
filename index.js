90dc45bae9cee95abdd5439f20815a1505d33f28
livikavesi <livitaufao@gmail.com>
2022-07-02 14:30:50 +1200

    main nav contact css

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-let blog = document.getElementById('blog');
-let core = document.getElementById('core');
-let home = document.getElementById('home');
-let about = document.getElementById('about');
+let blogBtn = document.getElementById('blog');
+let coreBtn = document.getElementById('core');
+let homeBtn = document.getElementById('home');
+let aboutBtn = document.getElementById('about');
+let contactBtn = document.getElementById('contacts');
 let mainImgContainer = document.getElementById('main-img-container');
 let secondImgContainer = document.getElementById('second-img-container');
 let thirdImgContainer = document.getElementById('third-img-container');
 let aboutContent = document.getElementById('about-content');
+let contactContent = document.getElementById('contact');
 
-core.onclick = () => {
+coreBtn.onclick = () => {
     mainImgContainer.style.display = 'none';
     secondImgContainer.style.display = 'flex';
     thirdImgContainer.style.display = 'none';
     aboutContent.style.display = 'none';
+    contactContent.style.display = 'none';
 }
-blog.onclick = () => {
+blogBtn.onclick = () => {
     mainImgContainer.style.display = 'none';
     secondImgContainer.style.display = 'none';
     thirdImgContainer.style.display = 'flex';
     thirdImgContainer.style.justifyContent = 'space-around';
     aboutContent.style.display = 'none';
+    contactContent.style.display = 'none';
 }
-home.onclick = () => {
+homeBtn.onclick = () => {
     secondImgContainer.style.display = 'none';
     thirdImgContainer.style.display = 'none';
     mainImgContainer.style.display = 'flex';
     aboutContent.style.display = 'none';
+    contactContent.style.display = 'none';
 }
-about.onclick = () => {
+aboutBtn.onclick = () => {
     secondImgContainer.style.display = 'none';
     thirdImgContainer.style.display = 'none';
     mainImgContainer.style.display = 'none';
     aboutContent.style.display = 'flex';
     aboutContent.style.flexDirection = 'column';
+    contactContent.style.display = 'none';
+}
+contactBtn.onclick = () => {
+    secondImgContainer.style.display = 'none';
+    thirdImgContainer.style.display = 'none';
+    mainImgContainer.style.display = 'none';
+    aboutContent.style.display = 'none';
+    contactContent.style.display = 'flex';
 }
