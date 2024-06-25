266489d9ac7b12902a4d92fe96e987d0a8740fc3
livikavesi <107158405+livikavesi@users.noreply.github.com>
2024-06-26 10:53:00 +1200

    fetch data

--- index.js
+++ index.js
@@ -47,3 +47,7 @@ contactBtn.onclick = () => {
     contactContent.style.display = 'flex';
 }
 
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
