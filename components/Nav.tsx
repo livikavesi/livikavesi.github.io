dcccae73d77a74495a48024e87bca50276503192
livikavesi <107158405+livikavesi@users.noreply.github.com>
2024-08-09 18:48:58 +1200

    resolve working site by adding database

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -89,13 +89,30 @@ const Nav = () => {
 
             {toggleDropdown && (
               <div className="dropdown">
-                <Link 
-                  href="/profile" 
+                <Link
+                  href="/profile"
                   className="dropdown_link"
                   onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
+                <Link
+                  href="/create-propt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
 
                 <button
                   type="button"
